70bf901018869ba6aba771bb3752bb7280a1bfca
1211101234 <zahinadri@gmail.com>
2025-08-12 11:22:42 +0800

    Added horsepower, top speed, and image URL fields to edit car form

--- src/app/components/edit-car/edit-car.ts
+++ src/app/components/edit-car/edit-car.ts
@@ -1,53 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CarService } from '../../services/cars';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { selectCarById } from '../../store/car.selector';
+import { CommonModule } from '@angular/common';
+import { editCar } from '../../store/car.actions';
+import { Car } from '../../models/car';
 
 @Component({
   selector: 'app-edit-car',
   standalone: true,
   templateUrl: './edit-car.html',
-  
-  imports: [ReactiveFormsModule]
+  imports: [ReactiveFormsModule, CommonModule]
 })
 export class EditCar implements OnInit {
   carForm!: FormGroup;
   carId!: number;
-  scrollService: any;
 
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
-    private carService: CarService,
+    private store: Store,
     private router: Router
   ) {}
 
   ngOnInit(): void {
     this.carId = +this.route.snapshot.paramMap.get('id')!;
-    const car = this.carService.getCarById(this.carId);
 
-    if (!car) {
-      alert('Car not found!');
-      this.router.navigate(['/']);
-      return;
-    }
+    this.store.select(selectCarById(this.carId)).subscribe((car) => {
+      if (!car) {
+        alert('Car not found!');
+        this.router.navigate(['/']);
+        return;
+      }
 
-    this.carForm = this.fb.group({
-      name: [car.name, Validators.required],
-      year: [car.year, Validators.required],
-      origin: [car.origin, Validators.required],
-      description: [car.description],
-      image: [car.image],
-      horsepower: [car.horsepower],
-      topSpeed: [car.topSpeed]
+      this.carForm = this.fb.group({
+        name: [car.name, Validators.required],
+        year: [car.year, Validators.required],
+        origin: [car.origin, Validators.required],
+        description: [car.description],
+        image: [car.image],
+        horsepower: [car.horsepower],
+        topSpeed: [car.topSpeed]
+      });
     });
   }
 
   onSubmit() {
     if (this.carForm.valid) {
-      this.carService.updateCar(this.carId, this.carForm.value);
+      const updatedCar: Car = {
+        id: this.carId,
+        ...this.carForm.value
+      };
+      this.store.dispatch(editCar({ car: updatedCar }));
       this.router.navigate(['/']);
     }
   }
